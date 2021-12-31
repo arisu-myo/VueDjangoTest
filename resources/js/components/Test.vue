3d9c1725fb74aa4100d43a00a5895015d2c749be
arisu <arisudayo@outlook.jp>
2022-01-01 08:40:32 +0900

    ver0.0.19

--- resources/js/components/Test.vue
+++ resources/js/components/Test.vue
@@ -1,25 +1,10 @@
 <template>
   <div>
     <div>
-      <!-- <video
-        id="video"
-        controls
-        autoplay
-        playsinline
-        width="80%"
-        height=""
-        @click="controlVideo"
-      ></video> -->
       <button @click="chengeVideo">test</button>
       <div style="width: 80%">
         <video-player :options="videoOptions" />
       </div>
-      <!-- <video controls width="80%">
-        <source
-          src="media/1599366a-e30c-42fd-b2e8-9b41a41186be/3d6a2eaafbf0463a89c8241532a4a648.mp4"
-        />
-      </video> -->
-      <!-- <button @click="this.videoPlay">video play</button> -->
     </div>
   </div>
 </template>
@@ -34,7 +19,6 @@
 </style>
 
 <script>
-import Hls from "hls.js";
 import VideoPlayer from "./videojs.vue";
 import axios from "axios";
 import { mapState } from "vuex";
@@ -45,36 +29,26 @@ export default {
   },
   data: () => {
     return {
-      video_url: null,
-      hls: new Hls({}),
-      videoflg1: true,
       videoOptions: {
         controls: true,
         responsive: true,
         autoplay: false,
         sources: [
           {
-            // withCredentials: false,
             type: "application/x-mpegURL",
-            // src: "/media/6b2ce74c-2e54-44e6-bc65-de88ccec641f/f5754471f7eb4ef8acc0370c35daab10/f5754471f7eb4ef8ac_1RiW1Q7.m3u8",
-            // src: "",
+            src: "/",
           },
         ],
       },
     };
   },
-  computed: {
-    // ...mapState({
-    //   VideoURL: (state) => state.pege.VideoURL,
-    // }),
-    createURL() {
-      axios.get("api/file/list").then((responce) => {
-        console.log(Object.values(responce.data)[0]);
-        this.props.sources[0].src = Object.values(responce.data)[0];
-        // this.VideoPlayer.src = Object.values(responce.data)[0];
-      });
-    },
+  mounted() {
+    axios.get("api/file/list").then((responce) => {
+      console.log(Object.values(responce.data)[0]);
+      this.videoOptions.sources[0].src = Object.values(responce.data)[0];
+    });
   },
+
   methods: {},
 };
 </script>
