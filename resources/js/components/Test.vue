190f38f0034a92c531cb5cf33d393c0319fa67bf
arisu <arisudayo@outlook.jp>
2022-03-14 03:57:16 +0900

    ver0.0.24

--- resources/js/components/Test.vue
+++ resources/js/components/Test.vue
@@ -18,43 +18,50 @@
 
       <div class="item2">
         <div v-if="douga_file != null">
-          <p>※印が入っている所は必須事項です</p>
+          <h4 class="h4-text">動画（プレビュー）</h4>
           <video class="douga" :src="douga_file" controls></video><br />
 
           <div>
-            <label class="rh_text">ファイル名※：</label><br />
+            <h4 class="h4-text">ファイル名(必須):</h4>
+
             <input
-              class="input__upload"
+              class="upload_file_name"
               type="text"
               v-model="file_title"
             /><br />
           </div>
 
-          <label class="text-left">説明：</label><br />
-          <input class="input__upload" type="text" v-model="file_ds" /><br />
+          <div>
+            <h4 class="h4-text">説明(任意):</h4>
 
-          <label>他の人に共有をする※：</label>
-          <input type="checkbox" />
+            <textarea
+              maxlength="200"
+              class="upload_ds"
+              v-model="file_ds"
+            /><br />
+          </div>
+
+          <div>
+            <h4 class="h4-text">他者への表示：</h4>
+            <label>共有を許可する</label>
+            <input v-model="public_flg" type="checkbox" />
+          </div>
+
+          <button class="btn btn-success uploadbtn" @click="fileUpload()">
+            アップロード
+          </button>
         </div>
 
-        <label>アップロードしたいファイルを選択してください</label><br />
-        <input type="file" accept="video/mp4,video/x-m4v" @change="setFile" />
+        <div v-if="douga_file == null">
+          <label>アップロードしたいファイルを選択してください</label><br />
+          <input type="file" accept="video/mp4,video/x-m4v" @change="setFile" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.input__upload {
-  width: 25vw;
-  height: 40px;
-  margin: 0.3em;
-}
-
-.rh_text {
-  padding-right: 15vw;
-}
-
 .hr-0 {
   padding: 0em;
   margin: 0em;
@@ -90,7 +97,7 @@
   }
 }
 /* @media only screen and (max-device-width: 1200px) {
-  スマホの場合のみ
+  スマホの場合のみ?
 } */
 
 .item1 {
@@ -107,26 +114,64 @@
 
 .douga {
   margin-top: 0.5em;
-  width: 30vw;
+  width: 50vw;
 }
+.upload_file_name {
+  width: 40vw;
+  height: 40px;
+  margin: 0.3em;
+}
+.upload_ds {
+  width: 40vw;
+  height: 120px;
+}
+.h4-text {
+  margin: 1em 9em 1em 9em;
+  padding: 0em 0em 0em 0.5em;
+  text-align: left;
+  border-left: 7px solid #3584bb;
+}
+.uploadbtn {
+  margin: 2em 0em 1em 0em;
+  padding: 1em 2em 1em 2em;
+}
+
 @media screen and (max-width: 727px) {
   .douga {
     margin-top: 0.5em;
     width: 100vw;
+  }
+  .upload_file_name {
+    width: 80vw;
+    height: 40px;
+    margin: 0.3em;
+  }
+  .upload_ds {
+    width: 80vw;
+    height: 120px;
+  }
+  .h4-text {
+    margin: 1em 0em 1em 1em;
+    padding: 0em 0em 0em 0.5em;
+    text-align: left;
+    border-left: 7px solid #3584bb;
   }
 }
 </style>
 
 
 <script>
+import axios from "axios";
 import { mapState } from "vuex";
 export default {
   data: () => {
     return {
       douga_file: null,
+      douga_data: null,
       file_title: "",
       file_ds: "",
       public_flg: false,
+      test: null,
     };
   },
   computed: {
@@ -151,8 +196,22 @@ export default {
       console.log(url);
 
       this.douga_file = url;
+      this.douga_data = file;
       this.file_title = file.name;
     },
+    fileUpload() {
+      const data = new FormData();
+      data.append("file", this.douga_data, "image.mp4");
+      data.append("file_name", this.file_title);
+      data.append("file_description", this.file_ds);
+      data.append("public", this.public_flg);
+      this.test = data;
+      this.$store.dispatch("pege/FileUpLoad", data);
+    },
+  },
+  beforeMount() {
+    let videotag = document.getElementsByTagName("video");
+    videotag.volume = 0.3;
   },
   watch: {
     LoginStatus: function (newVal, oldVal) {
--- resources/js/store/pege.js
+++ resources/js/store/pege.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import cookies from "vue-cookies";
+
 const state = {
     Loading: true,
 
@@ -14,6 +17,22 @@ const actions = {
     async LoadStatus(context, loading) {
         context.commit("setLoading", loading);
     },
+    async FileUpLoad(content, data) {
+        axios.defaults.xsrfCookieName = "csrftoken";
+        axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+
+        await axios
+            .post("api/file/upload/", data, {
+                headers: {
+                    "content-type": "multipart/form-data",
+                    Authorization: `JWT ${cookies.get("jwt")}`
+                }
+            })
+            .then((res) => {
+                window.location.href = "/test"
+            })
+            .catch((error) => console.log(error.response))
+    }
 
 }
 
